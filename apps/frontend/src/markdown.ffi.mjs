6548c3822a5b09ca5b46b24f8b4dfffec239f5f4
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-05-22 16:29:06 +0200

    fix: switch md parser to marked
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- apps/frontend/src/markdown.ffi.mjs
+++ apps/frontend/src/markdown.ffi.mjs
@@ -1,55 +1,19 @@
+import DOMPurify from 'dompurify'
 import hljs from 'highlight.js/lib/core'
-import showdown from 'showdown'
+import { Marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
+
+const parser = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
+    },
+  })
+)
 
 export function convert(content) {
-  const converter = new showdown.Converter({
-    extensions: [showdownHighlight({ pre: true, auto_detection: true })],
-    tasklists: true,
-  })
-  return converter.makeHtml(content)
-}
-
-const classAttr = 'class="'
-
-export function showdownHighlight({ pre = false, auto_detection = true } = {}) {
-  const filter = text => {
-    const params = {
-      left: '<pre><code\\b[^>]*>',
-      right: '</code></pre>',
-      flags: 'g',
-    }
-
-    const replacement = (wholeMatch, match, left, right) => {
-      const lang = (left.match(/class=\"([^ \"]+)/) || [])[1]
-      if (!lang && !auto_detection) {
-        return wholeMatch
-      }
-
-      if (left.includes(classAttr)) {
-        const attrIndex = left.indexOf(classAttr) + classAttr.length
-        left = left.slice(0, attrIndex) + 'hljs ' + left.slice(attrIndex)
-      } else {
-        left = left.slice(0, -1) + ' class="hljs">'
-      }
-
-      if (pre && lang) {
-        left = left.replace('<pre>', `<pre class="${lang} language-${lang}">`)
-      }
-
-      if (lang && hljs.getLanguage(lang)) {
-        return left + hljs.highlight(match, { language: lang }).value + right
-      }
-
-      return left + hljs.highlightAuto(match).value + right
-    }
-
-    return showdown.helper.replaceRecursiveRegExp(text, replacement, params.left, params.right, params.flags)
-  }
-
-  return [
-    {
-      type: 'output',
-      filter,
-    },
-  ]
+  const parsed = parser.parse(content)
+  return DOMPurify.sanitize(parsed, { USE_PROFILES: { html: true } })
 }

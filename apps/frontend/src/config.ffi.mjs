a915c764ec7cf97ae6203f4a429cb605f6a44784
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-07-17 18:43:16 +0200

    fix: re-add scroll on click

--- apps/frontend/src/config.ffi.mjs
+++ apps/frontend/src/config.ffi.mjs
@@ -2,12 +2,22 @@ export function is_dev() {
   return import.meta.env.DEV
 }
 
+function findChild(shadowRoot, id) {
+  for (const node of shadowRoot.querySelectorAll('search-result')) {
+    const elem = node.shadowRoot.getElementById(id)
+    if (elem) return elem
+  }
+}
+
 export function scrollTo(id) {
   const cache = document.getElementsByTagName('cache-signatures')
   if (!cache?.[0]) return
-  const elem = cache[0].shadowRoot.getElementById(id)
+  const elem = findChild(cache[0].shadowRoot, id)
+  if (!elem) return
   const elemRect = elem.getBoundingClientRect()
-  const navbarRect = document.getElementsByClassName('navbar')?.[0]?.getBoundingClientRect()
+  const navbarRect = document
+    .getElementsByClassName('navbar')?.[0]
+    ?.getBoundingClientRect()
   const bodyRect = document.body.getBoundingClientRect()
   const offset = elemRect.top - bodyRect.top - (navbarRect?.height ?? 0) - 12
   window.scrollTo({ top: offset, behavior: 'smooth' })
@@ -15,7 +25,11 @@ export function scrollTo(id) {
 
 export function captureMessage(content) {
   if (is_dev()) return content
-  if (typeof Sentry !== 'undefined' && Sentry?.captureMessage && typeof Sentry.captureMessage === 'function')
+  if (
+    typeof Sentry !== 'undefined' &&
+    Sentry?.captureMessage &&
+    typeof Sentry.captureMessage === 'function'
+  )
     Sentry.captureMessage(content)
   return content
 }
